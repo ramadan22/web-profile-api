0b8c8d3bfd869f86bfbedde48a9afd07cd811c91
ramadan22 <harisramadan22@gmail.com>
2024-03-31 21:19:13 +0700

    fix db host config mysql database

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
